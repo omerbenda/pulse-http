219e1bf4d972f9809f8e331ddfb1936a671b52b8
omerbenda <omerbenda@gmail.com>
2025-08-21 22:20:49 +0300

    feat(http-request-form.tsx): Add background color

--- src/pages/main-page/components/http-request-form/http-request-form.tsx
+++ src/pages/main-page/components/http-request-form/http-request-form.tsx
@@ -6,6 +6,7 @@ import {
   Box,
   Select,
   MenuItem,
+  Paper,
 } from '@mui/material';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { HTTPRequestInputs, HTTPRequestMethod } from './types';
@@ -47,70 +48,84 @@ const HTTPRequestForm = () => {
 
   return (
     <FormControl onSubmit={handleSubmit(onSubmit)} component="form" fullWidth>
-      <Box display="flex">
-        <Select
-          defaultValue={HTTPRequestMethod.GET}
-          sx={{ borderStartEndRadius: 0, borderEndEndRadius: 0 }}
-          {...register('method', { required: true })}
-        >
-          {Object.keys(HTTPRequestMethod).map((method) => (
-            <MenuItem key={method} value={method}>
-              {HTTPRequestMethod[method as keyof typeof HTTPRequestMethod]}
-            </MenuItem>
-          ))}
-        </Select>
-        <TextField
-          variant="outlined"
-          placeholder="URL"
-          fullWidth
-          slotProps={{
-            input: {
-              sx: {
-                borderColor: 'red',
-                borderStartStartRadius: 0,
-                borderEndStartRadius: 0,
+      <Paper
+        variant="elevation"
+        elevation={1}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100%',
+          gap: 2,
+          padding: 2,
+        }}
+      >
+        <Box display="flex">
+          <Select
+            defaultValue={HTTPRequestMethod.GET}
+            sx={{ borderStartEndRadius: 0, borderEndEndRadius: 0 }}
+            {...register('method', { required: true })}
+          >
+            {Object.keys(HTTPRequestMethod).map((method) => (
+              <MenuItem key={method} value={method}>
+                {HTTPRequestMethod[method as keyof typeof HTTPRequestMethod]}
+              </MenuItem>
+            ))}
+          </Select>
+          <TextField
+            variant="outlined"
+            placeholder="URL"
+            fullWidth
+            slotProps={{
+              input: {
+                sx: {
+                  borderColor: 'red',
+                  borderStartStartRadius: 0,
+                  borderEndStartRadius: 0,
+                },
               },
-            },
-          }}
-          {...register('url', { required: true })}
-        />
-      </Box>
-      <Box>
-        {Object.values(headers).map((header, index) => (
-          <Box display="flex" key={index}>
+            }}
+            {...register('url', { required: true })}
+          />
+        </Box>
+        <Paper variant="outlined" elevation={1} sx={{ padding: 2 }}>
+          {Object.values(headers).map((header, index) => (
+            <Box display="flex" key={index}>
+              <TextField
+                placeholder="Header"
+                variant="outlined"
+                fullWidth
+                onChange={(e) => changeHeaderName(index, e.target.value)}
+                onKeyDown={(e) => {
+                  e.key === 'Backspace' && !header && deleteHeader(index);
+                }}
+                value={header}
+                autoFocus
+              />
+              <TextField
+                placeholder="Value"
+                variant="outlined"
+                fullWidth
+                {...register(`headers.${header}`)}
+              />
+            </Box>
+          ))}
+          <Box display="flex">
             <TextField
               placeholder="Header"
-              variant="outlined"
               fullWidth
-              onChange={(e) => changeHeaderName(index, e.target.value)}
-              onKeyDown={(e) => {
-                e.key === 'Backspace' && !header && deleteHeader(index);
-              }}
-              value={header}
-              autoFocus
+              onChange={(e) => createHeader(e.target.value)}
+              value=""
+              disabled={headers[headers.length - 1] === ''}
             />
-            <TextField
-              placeholder="Value"
-              variant="outlined"
-              fullWidth
-              {...register(`headers.${header}`)}
-            />
+            <TextField placeholder="Value" fullWidth disabled />
           </Box>
-        ))}
-        <Box display="flex">
-          <TextField
-            placeholder="Header"
-            fullWidth
-            onChange={(e) => createHeader(e.target.value)}
-            value=""
-            disabled={headers[headers.length - 1] === ''}
-          />
-          <TextField placeholder="Value" fullWidth disabled />
+        </Paper>
+        <Box display="flex" justifyContent="flex-end">
+          <Button type="submit" variant="contained">
+            Send
+          </Button>
         </Box>
-      </Box>
-      <Button type="submit" variant="contained">
-        Send
-      </Button>
+      </Paper>
     </FormControl>
   );
 };
